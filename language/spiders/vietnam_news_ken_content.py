3b9474d8424de93d1aae5db761eedaa1a749314c
杨明明 <yangmingming@datatang.com>
2019-07-07 16:24:47 +0800

    '20190707a'

--- language/spiders/vietnam_news_ken_content.py
+++ language/spiders/vietnam_news_ken_content.py
@@ -4,12 +4,17 @@ from scrapy_redis.spiders import RedisSpider
 from language.items import NewsContentItem
 
 
-class VietnamNewsKenContentSpider(scrapy.Spider):
+class VietnamNewsKenContentSpider(RedisSpider):
     name = 'vietnam_news_ken_content'
     allowed_domains = ['kenh14.vn']
-    start_urls = [
-        'http://kenh14.vn/xuc-dong-nguoi-cha-o-sai-gon-om-con-khoc-trong-hanh-phuc-sau-hon-4-thang-tim-kiem-moi-mon-20190703172628968.chn'
-    ]
+    # start_urls = [
+        # 'http://kenh14.vn/xuc-dong-nguoi-cha-o-sai-gon-om-con-khoc-trong-hanh-phuc-sau-hon-4-thang-tim-kiem-moi-mon-20190703172628968.chn'
+        # "http://kenh14.vn/dan-sao-viet-cung-te-tuu-khoe-sac-tren-tham-do-su-kien-20170309230551411.chn"
+        # "http://kenh14.vn/cuoc-dua-ky-thu-cac-hoa-hau-tai-mat-vi-leo-60m-thang-day-st-binh-an-ve-dich-dau-tien-20190706223821038.chn"
+        # "http://kenh14.vn/10-dieu-nen-biet-ve-black-panther-2-ai-cung-hao-huc-mong-doi-so-4-vi-da-duoc-nha-hang-trong-endgame-2019070222305589.chn"
+        # "http://kenh14.vn/running-man-on-troi-lien-binh-phat-da-biet-choi-do-ma-con-tut-duoc-quan-tran-thanh-20190707000419163.chn"
+        # "http://kenh14.vn/ngoai-me-vu-so-khanh-ve-nha-di-con-van-con-2-cuc-pham-lay-lai-niem-tin-lay-chong-cho-chi-em-20190705163051741.chn"
+    # ]
 
     redis_key = 'vietnam_news_ken_link'
     custom_settings = {
