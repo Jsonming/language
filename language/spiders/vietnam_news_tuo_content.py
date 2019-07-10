54ea737e9fa8900a2d7bf2ba31ac985d8fefcd7a
杨明明 <yangmingming@datatang.com>
2019-07-10 10:34:36 +0800

    '20190710a'

--- language/spiders/vietnam_news_tuo_content.py
+++ language/spiders/vietnam_news_tuo_content.py
@@ -10,10 +10,12 @@ class VietnamNewsTuoContentSpider(RedisSpider):
     name = 'vietnam_news_tuo_content'
     allowed_domains = ['tuoitre.vn']
     start_urls = [
-        'https://tuoitre.vn/dia-don-mv-phim-ngan-cho-ngay-tinh-yeu-valentine-20190213160930321.htm'
+        # 'https://tuoitre.vn/vng-tu-nhung-buoc-di-esports-so-khoi-den-bom-tan-mobile-legends-bang-bang-20190612135605021.htm'
+        # 'https://tuoitre.vn/xe-csgt-binh-duong-gay-tai-nan-1-nguoi-tu-vong-20190609212738957.htm'
+        'https://tuoitre.vn/de-nghi-bac-tat-ca-khang-cao-vu-that-thoat-3-608-ti-o-ngan-hang-dong-a-20190529092940243.htm'
     ]
 
-    redis_key = 'vietnam_news_tuo_link'
+    redis_key = 'vietnam_news_tuo_content'
     custom_settings = {
         'REDIS_HOST': '47.105.132.57',
         'REDIS_PORT': 6379,
@@ -26,6 +28,8 @@ class VietnamNewsTuoContentSpider(RedisSpider):
     def parse(self, response):
         content = []
         content.extend(response.xpath('//div[@class="content fck"]/p//text()').extract())
+        content.extend(response.xpath('//div[@class="content fck"]/div//text()').extract())
+        content.extend(response.xpath('//div[@class="sp-detail-content"]/p//text()').extract())
 
         content = ' '.join(content).replace('\r', '').replace('\t', '').replace('\n', '').replace('\xa0', '')
         item = NewsContentItem()
